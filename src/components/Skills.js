dbd9ad81f45f8b9ebaa490b8ab7c62370b0d2df6
nimo a <nimoabdul@gmail.com>
2024-12-03 05:56:53 -0500

    fixed tools

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,96 +1,148 @@
-import React from 'react';
-import './Skills.css';
+import React from "react";
+import "./Skills.css";
 
 function Skills() {
   return (
     <section className="skills">
       <div className="skills-header">
-        <h1>What I Know </h1>
+        <h1>What I Know</h1>
         <p>
           With a strong foundation in modern technologies, I bring innovative solutions to the table, tailored to your needs.
         </p>
       </div>
       <div className="skills-container">
         <div className="skill">
-        <img
-            src="https://media.licdn.com/dms/image/v2/D5612AQGdi77S5VJa6w/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1718429090603?e=2147483647&v=beta&t=eabf24yvZ8JQa9CIGSmJlXq53BPuzFffClyQ7CZr67o" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
+          <img 
+            src="https://cdn3.emoji.gg/emojis/7253-staff-badge.png" 
+            alt="Continuous Support Icon" 
+            className="skill-icon" 
           />
-          <h3>Cloud Computing</h3>
+          <h3>Continuous Support</h3>
           <p>
-            Skilled in deploying and managing scalable solutions using AWS, Microsoft Azure, and Google Cloud Platform.
+          I advance web-based software to keep it efficient and competitive, ensuring quick updates and regular functional module releases.
+  
           </p>
         </div>
         <div className="skill">
-        <img
-            src="https://cdn-icons-png.flaticon.com/512/8522/8522214.png" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
+          <img 
+            src="https://cdn-icons-png.flaticon.com/256/470/470651.png" 
+            alt="Back-End Development Icon" 
+            className="skill-icon" 
           />
-          <h3>Computer Networks & Security</h3>
+          <h3>Back-End Development</h3>
           <p>
-            Proficient in securing systems and networks with expertise in firewalls, encryption, and vulnerability mitigation.
+          I implement business logic on the back end using proven frameworks and create well-structured APIs for integration with corporate systems.
           </p>
         </div>
         <div className="skill">
-        <img
-            src="https://ezitech.org/blogs/wp-content/uploads/2023/12/0_l97vC1QkxR8Sivxt.webp" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
+          <img 
+            src="https://code.visualstudio.com/assets/images/code-stable-white.png" 
+            alt="Front-End Design Icon" 
+            className="skill-icon" 
           />
-          <h3>Java</h3>
+          <h3>Front-End Design & Development</h3>
           <p>
-            Extensive experience in building applications with Java, focusing on Object-Oriented Programming and robust backend systems.
+          I design UI based on user needs and bring it to life using smart front-end technologies.
           </p>
         </div>
         <div className="skill">
-        <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/539px-React-icon.svg.png" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
+          <img 
+            src="https://img.icons8.com/ios7/600w/FFFFFF/artificial-intelligence.png" 
+            alt="AI Knowledge Icon" 
+            className="skill-icon" 
           />
-          <h3>React</h3>
+          <h3>Artificial Intelligence</h3>
           <p>
-            Skilled in creating dynamic and responsive web applications using React and its ecosystem.
-          </p>
-        </div>
-        <div className="skill">
-        <img
-            src="https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fskill_page%2F36894%2Flogo%2Foptimized%2Fsql-64a6e0f07773cf17581e76ca09e17dbc.png" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
-          />
-          <h3>Databases</h3>
-          <p>
-          Proficient in Oracle RDBMS, SQL Server, MySQL, and MongoDB, with experience in designing, querying, and managing databases to optimize performance and support data-driven solutions
-          </p>
-        </div>
-        <div className="skill">
-        <img
-            src="https://www.10bestdesign.com/blog/content/images/2018/03/20.png" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
-          />
-          <h3>Web Development</h3>
-          <p>
-          Skilled in JavaScript, JQuery, PHP, TypeScript, and Node.js, with expertise in building dynamic web applications, enhancing interactivity, and creating efficient backend solutions.
-          </p>
-        </div>
-        <div className="skill">
-        <img
-            src="https://download.logo.wine/logo/Linux/Linux-Logo.wine.png" // Replace with your image
-            alt="Systems Icon"
-            className="skill-icon"
-          />
-          <h3> Systems</h3>
-          <p>
-          Experienced in macOS, Windows, and Linux, with a strong ability to navigate, configure, and troubleshoot across diverse operating systems to support both development and deployment environment
+            I have a strong understanding of AI, using frameworks like TensorFlow and PyTorch to develop machine learning models for predictive analytics and problem-solving.
           </p>
         </div>
       </div>
+      <div className="tools-header">
+        <h2>Tools</h2>
+      </div>
+      <div className="tools-container">
+        <div className="tool">
+          <img
+            src="https://media.licdn.com/dms/image/v2/D5612AQGdi77S5VJa6w/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1718429090603?e=2147483647&v=beta&t=eabf24yvZ8JQa9CIGSmJlXq53BPuzFffClyQ7CZr67o" 
+            alt="Cloud Computing Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/8522/8522214.png" 
+            alt="Computer Networks & Security Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://ezitech.org/blogs/wp-content/uploads/2023/12/0_l97vC1QkxR8Sivxt.webp" 
+            alt="Java Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/539px-React-icon.svg.png" 
+            alt="React Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fskill_page%2F36894%2Flogo%2Foptimized%2Fsql-64a6e0f07773cf17581e76ca09e17dbc.png" 
+            alt="Databases Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2023/02/JavaScript-Logo.png" 
+            alt="Web Development Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png" 
+            alt="Systems Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://download.logo.wine/logo/Linux/Linux-Logo.wine.png" 
+            alt="Systems Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://amandeepmittal.gallerycdn.vsassets.io/extensions/amandeepmittal/pug/1.0.1/1509818475774/Microsoft.VisualStudio.Services.Icons.Default" 
+            alt="Systems Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png" 
+            alt="Systems Icon" 
+            className="tool-icon"
+          />
+        </div>
+        <div className="tool">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/1365px-Figma-logo.svg.png" 
+            alt="Systems Icon" 
+            className="tool-icon"
+          />
+        </div>
+      </div>
+      
       
     </section>
+    
   );
 }
 
